ddcc915c34300148256ba203fb0d77c887656135
Marcos Rodriguez <email.work.rgz@gmail.com>
2019-09-11 20:25:40 -0500

    Carga de proyecto
    Se realiza inicio del proyecto,
    
    Co-Authored-By: George <joragui2005@users.noreply.github.com>

---
+++ Cars/Controllers/IndexCarsController.cs
@@ -0,0 +1,106 @@
+﻿using Cars.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tek.Cars.BusCars;
+
+namespace Cars.Controllers
+{
+    public class IndexCarsController : Controller
+    {
+        private CarrosEF db = new CarrosEF();
+        private ProductosEFEntities dbp = new ProductosEFEntities();
+        // GET: IndexCars
+        public ActionResult Index()
+        {
+            ViewBag.GrupodeProducto = new SelectList(dbp.Grupo, "id_grup", "nombre_grup");
+            List<Cars.Models.Marca> marca = new List<Cars.Models.Marca>();
+            ViewBag.MarcadeProducto = new SelectList(marca, "id_marca", "nombre_marca");
+            List<Cars.Models.Producto> producto = new List<Cars.Models.Producto>();
+            ViewBag.Producto = new SelectList(producto, "id_prod", "nombre_prod");
+            List<Tek.Cars.BusCars.Carro> carros = new BusCars().GetCarros();
+            ViewBag.GrupodeProductohtml = new SelectList(dbp.Grupo, "id_grup", "nombre_grup");
+            return View(carros);
+        }
+        [HttpPost]
+        public ActionResult Index(string GrupodeProducto, string MarcadeProducto)
+        {
+            if (!string.IsNullOrEmpty(GrupodeProducto) && !string.IsNullOrEmpty(MarcadeProducto))
+            {
+                int idgpo = Convert.ToInt32(GrupodeProducto);
+                ViewBag.GrupodeProducto = new SelectList(dbp.Grupo, "id_grup", "nombre_grup");
+                List<Cars.Models.Marca> marca = dbp.Marca.Where(m => m.id_grup_marca == idgpo).ToList();
+                ViewBag.MarcadeProducto = new SelectList(marca, "id_marca", "nombre_marca");
+                int idmar = Convert.ToInt32(MarcadeProducto);
+                List<Cars.Models.Producto> producto = (dbp.Producto.Where(p => p.id_marca_prod == idmar).ToList());
+                ViewBag.Producto = new SelectList(producto, "id_prod", "nombre_prod");
+                ViewBag.GrupodeProductohtml = new SelectList(dbp.Grupo, "id_grup", "nombre_grup");
+            }
+            else if (!string.IsNullOrEmpty(GrupodeProducto) && string.IsNullOrEmpty(MarcadeProducto))
+            {
+
+                int idgpo = Convert.ToInt32(GrupodeProducto);
+                ViewBag.GrupodeProducto = new SelectList(dbp.Grupo, "id_grup", "nombre_grup");
+                List<Cars.Models.Marca> marca = dbp.Marca.Where(m => m.id_grup_marca == idgpo).ToList();
+                ViewBag.MarcadeProducto = new SelectList(marca, "id_marca", "nombre_marca");
+                ViewBag.GrupodeProductohtml = new SelectList(dbp.Grupo, "id_grup", "nombre_grup");
+
+                List<Cars.Models.Producto> producto = new List<Cars.Models.Producto>();
+                ViewBag.Producto = new SelectList(producto, "id_prod", "nombre_prod");
+            }
+            else
+            {
+                ViewBag.GrupodeProducto = new SelectList(dbp.Grupo, "id_grup", "nombre_grup");
+                List<Cars.Models.Marca> marca = new List<Cars.Models.Marca>();
+                ViewBag.MarcadeProducto = new SelectList(marca, "id_marca", "nombre_marca");
+                List<Cars.Models.Producto> producto = new List<Cars.Models.Producto>();
+                ViewBag.Producto = new SelectList(producto, "id_prod", "nombre_prod");
+                ViewBag.GrupodeProductohtml = new SelectList(dbp.Grupo, "id_grup", "nombre_grup");
+            }
+            List<Tek.Cars.BusCars.Carro> carros = new BusCars().GetCarros();
+            return View(carros);
+
+        }
+        public ActionResult GetGrupos(int id)
+        {
+            return Json(dbp.Marca.Where(m => m.id_grup_marca == id).ToList(), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult GetMarcas(int id)
+        {
+            return Json(dbp.Producto.Where(p => p.id_marca_prod == id).ToList(), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult About()
+        {
+            List<Tek.Cars.BusCars.Carro> carros = new BusCars().GetCarros();
+            return View(carros);
+        }
+        [HttpPost]
+        public ActionResult Create(Cars.Models.Carro car)
+        {
+            if (car.id_car == 0)
+            {
+                var archivo = Request.Files[0];
+                archivo.SaveAs(Server.MapPath("~/images/" + archivo.FileName));
+                db.Carro.Add(car);
+                db.SaveChanges();
+            }
+            else
+            {
+                var archivos = Request.Files;
+                if (archivos.Count > 0)
+                {
+                    var archivo = Request.Files[0];
+                    archivo.SaveAs(Server.MapPath("~/images/" + archivo.FileName));
+                }
+                db.Entry(car).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            Response.StatusCode = 200;
+            Response.StatusDescription = "Objeto Creado";
+            return Json(car, "application/jason", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Cars/Controllers/ProductoesEFController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cars.Models;
+
+namespace Cars.Controllers
+{
+    public class ProductoesEFController : Controller
+    {
+        private ProductosEFEntities db = new ProductosEFEntities();
+
+        // GET: ProductoesEF
+        public ActionResult Index()
+        {
+            var producto = db.Producto.Include(p => p.Grupo).Include(p => p.Marca);
+            return View(producto.ToList());
+        }
+
+        // GET: ProductoesEF/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // GET: ProductoesEF/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_grup_prod = new SelectList(db.Grupo, "id_grup", "nombre_grup");
+            ViewBag.id_marca_prod = new SelectList(db.Marca, "id_marca", "nombre_marca");
+            return View();
+        }
+
+        // POST: ProductoesEF/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_prod,nombre_prod,id_marca_prod,id_grup_prod")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Producto.Add(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_grup_prod = new SelectList(db.Grupo, "id_grup", "nombre_grup", producto.id_grup_prod);
+            ViewBag.id_marca_prod = new SelectList(db.Marca, "id_marca", "nombre_marca", producto.id_marca_prod);
+            return View(producto);
+        }
+
+        // GET: ProductoesEF/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_grup_prod = new SelectList(db.Grupo, "id_grup", "nombre_grup", producto.id_grup_prod);
+            ViewBag.id_marca_prod = new SelectList(db.Marca, "id_marca", "nombre_marca", producto.id_marca_prod);
+            return View(producto);
+        }
+
+        // POST: ProductoesEF/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_prod,nombre_prod,id_marca_prod,id_grup_prod")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(producto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_grup_prod = new SelectList(db.Grupo, "id_grup", "nombre_grup", producto.id_grup_prod);
+            ViewBag.id_marca_prod = new SelectList(db.Marca, "id_marca", "nombre_marca", producto.id_marca_prod);
+            return View(producto);
+        }
+
+        // GET: ProductoesEF/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: ProductoesEF/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Producto producto = db.Producto.Find(id);
+            db.Producto.Remove(producto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
