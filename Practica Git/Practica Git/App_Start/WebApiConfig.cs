5ecf554cbf9e6038f92918ad68d87c8bf3393f46
George <jorge.bravo77@gmail.com>
2019-09-16 18:21:01 -0700

    Probando Git desde VS

--- Practica Git/Practica Git/App_Start/WebApiConfig.cs
+++ Practica Git/Practica Git/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Practica_Git.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -14,11 +15,14 @@ namespace Practica_Git
             // Rutas de API web
             config.MapHttpAttributeRoutes();
 
+            config.MessageHandlers.Add(new TokenValidationHandler());
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
         }
     }
 }
