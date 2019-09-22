d83905531189a9adfab423a0d97313de7e0a9462
Marcos Rodriguez <email.work.rgz@gmail.com>
2019-09-22 18:31:59 -0500

    Carga de Template
    Se realiza carga de template ("Arquitectura_Moderna") el cual trabaja con
    - Bootstrap 4
    - jQuery 3.3.1
    operando en su totalidad.

---
+++ ArquitecturaModerna/ArquitecturaModerna/Controllers/ArquitecturaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArquitecturaModerna.Controllers
+{
+    public class ArquitecturaController : Controller
+    {
+        // GET: Arquitectura
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Proyectos()
+        {
+            return View();
+        }
+        public ActionResult Servicios()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+    }
+}
