fd6be8c59ed377f0958c80e60e713f26d10e9cac
George <jorge.bravo77@gmail.com>
2019-10-05 09:35:11 -0700

    Angular Blogs
    Primera parte

---
+++ ArquitecturaModerna/ArquitecturaModerna/Scripts/blogs.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+
+var url = location.origin + '/api/blog';
+(function () {
+    angular
+        .module("MyAPI", [])
+        .controller("BlogController", ['$http', function ($http) {
+            var $scope = this;
+            $scope.blogs = [];
+
+            $scope.CargaInicial = function () {
+                $http({
+                    url: url,
+                    method: "GET",
+                    headers: { Authorization: 'Basic U3VibmV0OjY3ODk=' },
+                    data: {}
+                }).then(function (response) {
+                    console.log(response);
+                    $scope.blogs = response.data;
+                }, function (error) {
+                    console.log(error);
+                });
+            };
+        }]);
+})();
